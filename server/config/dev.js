8858bfb6ffd8dda904f151ccfa3c8e89753209f1
Emerald Stanley <78609482+Gltched@users.noreply.github.com>
2021-04-19 19:44:14 -0700

    lol

--- server/config/dev.js
+++ server/config/dev.js
@@ -1,6 +1,6 @@
 module.exports = {
   mongoURI:
-    "mongodb+srv://Gltch:<CBijvVzu7iWF2194>@cluster0.qf8ml.mongodb.net/gltched?retryWrites=true&w=majority"
+    "mongodb+srv://Gltch:CBijvVzu7iWF2194@cluster0.qf8ml.mongodb.net/gltched?retryWrites=true&w=majority"
 };
 
 // CBijvVzu7iWF2194
